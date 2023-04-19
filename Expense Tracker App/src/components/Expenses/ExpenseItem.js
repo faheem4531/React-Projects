dc798fe9a659055ccb46841a77528ca84e707ed0
faheem4531 <aa0561085@gmail.com>
2023-04-19 06:43:43 +0500

    Create NewExpense Component

--- Expense Tracker App/src/components/Expenses/ExpenseItem.js
+++ Expense Tracker App/src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
+  const [title, setTitle] = useState(props.title);
+  function handleClick() {
+    setTitle("Updated");
+  }
 
   return (
     <Card className="expense-item">
@@ -13,9 +16,10 @@ function ExpenseItem(props) {
         date={props.date}
       />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Chnage Name</button>
     </Card>
   )
 }
