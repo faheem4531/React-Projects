458bb82c5d5f46ea82580c415024914f81e050b4
faheem4531 <aa0561085@gmail.com>
2023-05-07 12:46:13 +0500

    introduce style.modules.css

--- Set Goals App/src/components/CourseGoals/CourseInput/CourseInput.js
+++ Set Goals App/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,43 +1,43 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-import styled from 'styled-components';
+import classes from './CourseInput.module.css';
+// import styled from 'styled-components';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc;
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
 
-&.isvalid input {
-  border-color: red;
-  background-color: #ffdfdf;
-}
+// &.isvalid input {
+//   border-color: red;
+//   background-color: #ffdfdf;
+// }
 
-&.isvalid label {
-  color: red;
-}
-`
+// &.isvalid label {
+//   color: red;
+// }
+// `
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -61,11 +61,11 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && 'isvalid'}>
+      <div className={`${classes['form-control']} ${!isValid && classes.isvalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler}
         />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
