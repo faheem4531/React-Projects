d9bed758df528f4cbcba7f6547b0d844725aac54
faheem4531 <aa0561085@gmail.com>
2023-05-12 01:36:41 +0500

    Error component

--- Profile Keeper App/src/components/Users/AddUser.js
+++ Profile Keeper App/src/components/Users/AddUser.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 
 import CardWarp from "../UI/CardWarp";
+import ErrorModal from "../UI/ErrorModal";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css"
 
 const AddUser = (props) => {
   const [enterUsername, setEnterUsername] = useState('');
   const [enterUserAge, setEnterUserAge] = useState('');
+  const [error, setError] = useState();
+
 
   function addUserHandler(event) {
     event.preventDefault();
     if (enterUsername.trim().length === 0 || enterUserAge.trim().length === 0) {
+      setError({ title: 'Invalid input', message: 'Please enter a valid name and age' });
       return;
     }
     if (enterUserAge < 1) {
+      setError({ title: 'Invalid age', message: 'please enter a valid age (> 0).' });
       return;
     }
     props.onAddUser(enterUsername, enterUserAge);
@@ -30,25 +35,28 @@ const AddUser = (props) => {
   }
 
   return (
-    <CardWarp className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enterUsername}
-          onChange={handleUsername}
-        />
-        <label htmlFor="age">Age</label>
-        <input
-          id="age"
-          type="number"
-          value={enterUserAge}
-          onChange={handleUserAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </CardWarp>
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} />}
+      <CardWarp className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enterUsername}
+            onChange={handleUsername}
+          />
+          <label htmlFor="age">Age</label>
+          <input
+            id="age"
+            type="number"
+            value={enterUserAge}
+            onChange={handleUserAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </CardWarp>
+    </div>
   )
 }
 
