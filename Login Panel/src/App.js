003494716f960e2ff0bbc2d2ed2bbbc82ae2f368
faheem4531 <aa0561085@gmail.com>
2023-05-14 02:17:17 +0500

    useContext hook - passing functions

--- Login Panel/src/App.js
+++ Login Panel/src/App.js
@@ -29,7 +29,8 @@ function App() {
 
   return (
     <AuthContext.Provider value={{
-      isLoggedIn: isLoggedIn
+      isLoggedIn: isLoggedIn,
+      onLogout: logoutHandler
     }}>
       <MainHeader onLogout={logoutHandler} />
       <main>
