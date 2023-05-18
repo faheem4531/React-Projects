20a0278ef44e12e4e40443f54f14dca7651b0cf7
faheem4531 <aa0561085@gmail.com>
2023-05-19 02:21:53 +0500

    header card button

--- Order Meal App/src/components/Layout/Header.js
+++ Order Meal App/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 
 import mealImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCardButton from './HeaderCardButton';
 
 const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals Chosen by Everyone</h1>
-        <button>card</button>
+        <HeaderCardButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealImg} alt='table full of food you like!' />
