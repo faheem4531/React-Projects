c51a1b285db48fc482399e0885ed5c2de630138e
faheem4531 <aa0561085@gmail.com>
2023-05-21 05:14:26 +0500

    evaluate number of items to show on cart btn

--- Order Meal App/src/components/Layout/HeaderCardButton.js
+++ Order Meal App/src/components/Layout/HeaderCardButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from './HeaderCardButton.module.css';
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = props => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentNmber, items) => {
+    return currentNmber + items.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Card</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
