3e8fb8c845a5b5142d0a96bcda594532b9d16874
faheem4531 <aa0561085@gmail.com>
2023-05-21 16:54:58 +0500

    useReducer

--- Order Meal App/src/store/CartProvider.js
+++ Order Meal App/src/store/CartProvider.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-function addItemToCartHandler(items) { };
+const defaultCartState = {
+  items: '',
+  totalAmount: 0
+};
 
-function removeItemToCartHandler(id) { };
+function cartReducer(state, action) {
+  if (state.type === 'ADD') {
+    const updateItems = state.items.concat(action.item);
+    const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updateItems,
+      totalAmount: updateTotalAmount
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = props => {
 
+  const [cartState, dispatchartState] = useReducer(cartReducer, defaultCartState);
+
+  function addItemToCartHandler(items) {
+    dispatchartState({ type: 'ADD', item: items });
+  };
+
+  function removeItemToCartHandler(id) { };
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItems: addItemToCartHandler,
     removeItems: removeItemToCartHandler
   }
