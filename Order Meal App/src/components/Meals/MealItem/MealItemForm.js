6b720f2bfc9139dfe3e38354983669f1aece1a3f
faheem4531 <aa0561085@gmail.com>
2023-05-21 23:36:37 +0500

    useref and forwardRef

--- Order Meal App/src/components/Meals/MealItem/MealItemForm.js
+++ Order Meal App/src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,33 @@
+import React, { isValidElement, useRef, useState } from 'react';
+
+
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
 const MealItemForm = props => {
+  const [amountIsValid, setAmountIsValild] = useState(true);
+  const amountInputRef = useRef()
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;  // to convert string number into number (change data type)
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber > 5 || enteredAmountNumber < 1) {
+      setAmountIsValild(false);
+      return; // if condition unmatch min-max items then return the condition
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
         input={{
+          ref: { amountInputRef },
           id: 'amount_' + props.id,
           type: 'number',
           min: '1',
@@ -16,6 +37,7 @@ const MealItemForm = props => {
         }}
       />
       <button >+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   )
 }
