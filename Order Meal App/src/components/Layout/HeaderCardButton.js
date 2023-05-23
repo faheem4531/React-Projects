f29ef09e3b160cf3d99358f26b92b80da2f7ad4e
faheem4531 <aa0561085@gmail.com>
2023-05-24 02:27:45 +0500

    Add animation using useEffect

--- Order Meal App/src/components/Layout/HeaderCardButton.js
+++ Order Meal App/src/components/Layout/HeaderCardButton.js
@@ -1,18 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import classes from './HeaderCardButton.module.css';
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = props => {
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNmber, items) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((currentNmber, items) => {
     return currentNmber + items.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`
+
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
