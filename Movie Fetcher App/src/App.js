a6d26fbbfab6041de197f1011a3724c9c99eced5
faheem4531 <aa0561085@gmail.com>
2023-05-26 02:28:53 +0500

    use useEffect to send http req. when compo. reload

--- Movie Fetcher App/src/App.js
+++ Movie Fetcher App/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -10,7 +10,7 @@ function App() {
   const [error, setError] = useState(null);
 
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async function () {
     setIsLoading(true);
     setError(null);
     try {
@@ -34,7 +34,13 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false);
-  };
+  }, []);
+
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+
 
   let content = <p>Found no movies.</p>
 
