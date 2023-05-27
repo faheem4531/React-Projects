1a2e8b20c66b3e3d60f23ee50037e7dd20c0459a
faheem4531 <aa0561085@gmail.com>
2023-05-27 05:22:16 +0500

    just getting the values and console.log

--- Form Validator/src/components/SimpleInput.js
+++ Form Validator/src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import React, { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef('');
+  const [enteredName, setEnteredName] = useState('');
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    if (enteredName.trim() === '') {
+      return;
+    }
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName('');
+  }
+
+  function onChangeHandler(event) {
+    setEnteredName(event.target.value);
+
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={onChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
