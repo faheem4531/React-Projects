686d253adddedc0208b7646a4b3fd9093f2b6384
faheem4531 <aa0561085@gmail.com>
2023-06-02 03:18:41 +0500

    header cart total quantity functionality...

--- Redux/ShopSleek App/src/components/Shop/Products.js
+++ Redux/ShopSleek App/src/components/Shop/Products.js
@@ -18,13 +18,19 @@ const Dummy_products = [
     id: 'p3',
     price: 5,
     title: 'Denim Shirts',
-    description: 'Unleash your rugged charm with our durable and versatile denim shirts.'
+    description: 'Unleash your rugged charm with our durable denim shirts.'
   },
   {
     id: 'p4',
     price: 12,
     title: 'Graphic Tees',
     description: 'Make a statement with our bold and expressive graphic tees.'
+  },
+  {
+    id: 'p5',
+    price: 6,
+    title: 'Casual Comfort',
+    description: 'Relaxed and comfortable shirts for everyday wear.'
   },
 ];
 
@@ -33,14 +39,15 @@ const Products = (props) => {
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {Dummy_products.map(products => {
+        {Dummy_products.map((products) => (
           <ProductItem
             key={products.id}
+            id={products.id}
             title={products.title}
             price={products.price}
             description={products.description}
-          />
-        })}
+          />)
+        )}
 
       </ul>
     </section>
