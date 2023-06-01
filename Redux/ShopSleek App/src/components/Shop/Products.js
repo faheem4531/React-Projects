a6625badceb6200aade7f330d28821966a1c6635
faheem4531 <aa0561085@gmail.com>
2023-06-02 02:58:53 +0500

    add dummy products static array data

--- Redux/ShopSleek App/src/components/Shop/Products.js
+++ Redux/ShopSleek App/src/components/Shop/Products.js
@@ -1,16 +1,47 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const Dummy_products = [
+  {
+    id: 'p1',
+    price: 4,
+    title: 'Dress Shirt',
+    description: 'Elevate your formal attire with our impeccably tailored dress shirts.'
+  },
+  {
+    id: 'p2',
+    price: 7,
+    title: 'Flannel Shirts',
+    description: 'Stay cozy and fashionable with our soft and comfortable flannel shirts.'
+  },
+  {
+    id: 'p3',
+    price: 5,
+    title: 'Denim Shirts',
+    description: 'Unleash your rugged charm with our durable and versatile denim shirts.'
+  },
+  {
+    id: 'p4',
+    price: 12,
+    title: 'Graphic Tees',
+    description: 'Make a statement with our bold and expressive graphic tees.'
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='SharpThreads'
-          price={6}
-          description='Elevate your style with our impeccable collection of shirts.'
-        />
+        {Dummy_products.map(products => {
+          <ProductItem
+            key={products.id}
+            title={products.title}
+            price={products.price}
+            description={products.description}
+          />
+        })}
+
       </ul>
     </section>
   );
