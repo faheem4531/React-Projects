599ffe4adaa4d1414410cd08bfa5f441d5777566
faheem4531 <aa0561085@gmail.com>
2023-06-03 04:49:59 +0500

    work with firebase db to put data and noti. handle

--- Redux/ShopSleek App/src/App.js
+++ Redux/ShopSleek App/src/App.js
@@ -1,17 +1,65 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './components/store/ui-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function sendCartData() {
+
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Pending...',
+        message: 'pending cart data.'
+      }));
+
+      const response = await fetch('https://shopsleek-dd462-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data fail.');
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'sucessfull',
+        title: 'Sucessfull',
+        message: 'sent cart data sucessfully!'
+      }));
+
+    }
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'sending cart data fail.'
+      }));
+    })
+  }, [cart, dispatch]);
+
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- Redux/ShopSleek App/src/components/store/cart-slice.js
+++ Redux/ShopSleek App/src/components/store/cart-slice.js
@@ -7,6 +7,11 @@ const cartSlice = createSlice({
     totalQuantity: 0
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload
       const existingItem = state.items.find(item => item.id === newItem.id);
@@ -25,6 +30,7 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
+
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
