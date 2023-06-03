73478994e722ffa20de82a5086c7afcd57d15827
faheem4531 <aa0561085@gmail.com>
2023-06-03 06:04:45 +0500

    fetch stored data from db

--- Redux/ShopSleek App/src/App.js
+++ Redux/ShopSleek App/src/App.js
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Fragment, useEffect } from 'react';
 import { uiActions } from './components/store/ui-slice';
+import { cartActions } from './components/store/cart-slice';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
+
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
@@ -14,7 +18,32 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://shopsleek-dd462-default-rtdb.firebaseio.com/cart.json');
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data');
+      }
+
+      const cartData = await response.json();
+
+      dispatch(cartActions.replaceCart(cartData));
+    }
+
+    fetchData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Sending cart data fail.'
+      }));
+    });
+  }, [dispatch]);
+
+  useEffect(() => {
     async function sendCartData() {
+      if (isInitial) {
+        isInitial = false;
+        return;
+      }
 
       dispatch(uiActions.showNotification({
         status: 'pending',
@@ -32,19 +61,19 @@ function App() {
       }
 
       dispatch(uiActions.showNotification({
-        status: 'sucessfull',
+        status: 'success',
         title: 'Sucessfull',
-        message: 'sent cart data sucessfully!'
+        message: 'Sent cart data successfully!'
       }));
-
     }
     sendCartData().catch(error => {
       dispatch(uiActions.showNotification({
         status: 'error',
         title: 'Error',
-        message: 'sending cart data fail.'
+        message: 'Sending cart data fail.'
       }));
     })
+
   }, [cart, dispatch]);
 
 
